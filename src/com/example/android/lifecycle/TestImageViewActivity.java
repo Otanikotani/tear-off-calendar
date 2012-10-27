5c6e188cfa3e11126d83a8c1a7c7910645d783fa
Otanikotani <otanikotani@gmail.com>
2012-10-27 21:05:32 +0400

    Added limit for image moving

--- src/com/example/android/lifecycle/TestImageViewActivity.java
+++ src/com/example/android/lifecycle/TestImageViewActivity.java
@@ -1,14 +1,15 @@
 package com.example.android.lifecycle;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.example.android.lifecycle.util.StatusTracker;
 
@@ -17,12 +18,20 @@ public class TestImageViewActivity extends Activity implements OnTouchListener {
 	private StatusTracker mStatusTracker = StatusTracker.getInstance();
 	private ImageView imageToMove;
 
-	int mX, mY;
+	private float bottomLimitToMove;
+	float mX, mY;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test_image_view);
+
+		// Get screen height to determine when the limit
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		bottomLimitToMove = (float) (size.y - size.y * 0.50);
+		Log.w("SCREEN HEIGHT:", String.valueOf(bottomLimitToMove));
 
 		Log.w("Tag", "On create");
 		imageToMove = (ImageView) findViewById(R.id.imageView1);
@@ -34,26 +43,17 @@ public class TestImageViewActivity extends Activity implements OnTouchListener {
 		getMenuInflater().inflate(R.menu.activity_test_image_view, menu);
 		return true;
 	}
+	
+	
 
 	public boolean onTouch(View view, MotionEvent event) {
     	mStatusTracker.setStatus("TestImageViewActivity", "On touch!");
     	
-        final int x = (int) event.getRawX();
-        final int y = (int) event.getRawY();
+        final float x = event.getRawX();
+        final float y = event.getRawY();
         
-        StringBuilder coordinatesToString = new StringBuilder();
-        coordinatesToString.append(x);
-        coordinatesToString.append(", ");
-        coordinatesToString.append(y);
-        String xy = coordinatesToString.toString();
-        Log.w("Coordinates:", xy);
-        
-        StringBuilder imageCoordinatesToString = new StringBuilder();
-        imageCoordinatesToString.append(imageToMove.getX());
-        imageCoordinatesToString.append(", ");
-        imageCoordinatesToString.append(imageToMove.getY());
-        String imageXY = imageCoordinatesToString.toString();
-        Log.w("Image view coordinates: ", imageXY);
+//        printCoordinates(x, y);
+//        printImageCoordinates();
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
             	Log.w("Touch:", "Action down!");
@@ -64,7 +64,7 @@ public class TestImageViewActivity extends Activity implements OnTouchListener {
             	Log.w("Touch:", "Action up!");
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
-            	Log.w("Touch:", "Action pointer down!");
+
                 break;
             case MotionEvent.ACTION_POINTER_UP:
             	Log.w("Touch:", "Action pointer up!");
@@ -73,12 +73,28 @@ public class TestImageViewActivity extends Activity implements OnTouchListener {
             	float deltaY = y - mY;
             	imageToMove.setY(deltaY);
             	imageToMove.invalidate();
-            	Log.w("Touch:", "Action move!");
             	StringBuilder yDifferenceToString = new StringBuilder();
             	yDifferenceToString.append(deltaY);
-            	Log.w("Touch:", yDifferenceToString.toString());
+            	
+//            	printImageCoordinates();
+            	Log.w("Image y:", String.valueOf(imageToMove.getY()));
+            	if (imageToMove.getY() >= bottomLimitToMove) {
+            		Log.w("Touch:", "Go to another activity!");
+            	}            	
                 break;
         }    	
     	return true;
     }
+	
+	private void printCoordinates(float x, float y) {
+        StringBuilder coordinatesToString = new StringBuilder();
+        coordinatesToString.append(x);
+        coordinatesToString.append(", ");
+        coordinatesToString.append(y);
+        Log.w("Coordinates: ", coordinatesToString.toString());
+	}
+	
+	private void printImageCoordinates() {
+		printCoordinates(imageToMove.getX(), imageToMove.getY());
+	}
 }
